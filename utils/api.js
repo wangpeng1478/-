826e9af42bfbb802e8d509b461704f13390a2b1e
wangpeng1478 <wang760635994@outlook.com>
2018-06-15 10:58:40 +0800

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -1,20 +1,20 @@
 
 
-var index = "https://www.yuebanyuehao.com/laravel/public/wx/index?cate=indexadd";
+var index = "https://www.*********************.com/laravel/public/wx/index?cate=indexadd";
 
-var soso = "https://www.yuebanyuehao.com/laravel/public/wx/index?cate=page";
+var soso = "https://www.*********************.com/laravel/public/wx/index?cate=page";
 
-var page = "https://www.yuebanyuehao.com/laravel/public/wx/index?cate=page1";
+var page = "https://www.*********************.com/laravel/public/wx/index?cate=page1";
 
-var pagephone = "https://www.yuebanyuehao.com/laravel/public/wx/index?cate=tryst1";
+var pagephone = "https://www.*********************.com/laravel/public/wx/index?cate=tryst1";
 //大楼预约
-var pagex = "https://www.yuebanyuehao.com/laravel/public/wx/index?cate=page2";
+var pagex = "https://www.*********************.com/laravel/public/wx/index?cate=page2";
 
-var pagexphone = "https://www.yuebanyuehao.com/laravel/public/wx/index?cate=tryst2";
+var pagexphone = "https://www.*********************.com/laravel/public/wx/index?cate=tryst2";
 //房源预约
-var Entrust = "https://www.yuebanyuehao.com/laravel/public/wx/index?cate=tryst3";
+var Entrust = "https://www.*********************.com/laravel/public/wx/index?cate=tryst3";
 //委托
-var adviser = "https://www.yuebanyuehao.com/laravel/public/wx/index?cate=tryst4";
+var adviser = "https://www.*********************.com/laravel/public/wx/index?cate=tryst4";
 //投放
 
 module.exports = {
